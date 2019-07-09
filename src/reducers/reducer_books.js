eebb33707cde5d647fac019be55fc7b89c89f81c
Gemma Massó <gmasso@visual-engin.com>
2019-07-09 12:44:02 +0200

    Implemented book selection application

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+    return [
+        { title: 'Javascript', pages: 101 },
+        { title: 'Harry Potter', pages: 56 },
+        { title: 'The Dark Tower', pages: 89 },
+        { title: 'Eloquent Ruby', pages: 2 }
+    ]
+}
